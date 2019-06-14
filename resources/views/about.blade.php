2b142ebf6b27cce319dbc2e6f1f9849fc8e3aaa4
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2019-06-14 10:50:16 +0530

    Init

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+@section('content')
+    <h1>About Us</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ducimus. Quis laborum vel eligendi recusandae adipisci voluptatibus, totam dicta distinctio 
+    nesciunt rerum sapiente illo odio, praesentium est incidunt repellat non?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ducimus. Quis laborum vel eligendi recusandae adipisci voluptatibus, totam dicta distinctio 
+    nesciunt rerum sapiente illo odio, praesentium est incidunt repellat non?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ducimus. Quis laborum vel eligendi recusandae adipisci voluptatibus, totam dicta distinctio 
+    nesciunt rerum sapiente illo odio, praesentium est incidunt repellat non?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ducimus. Quis laborum vel eligendi recusandae adipisci voluptatibus, totam dicta distinctio 
+    nesciunt rerum sapiente illo odio, praesentium est incidunt repellat non?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, ducimus. Quis laborum vel eligendi recusandae adipisci voluptatibus, totam dicta distinctio 
+    nesciunt rerum sapiente illo odio, praesentium est incidunt repellat non?</p>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+@extends('layout')
+@section('content')
+    <h1>My First Website </h1>
+@endsection
